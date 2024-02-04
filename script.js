db748a52703ec26835be525538d3d05c06a54356
Andrew <aldortheold@gmail.com>
2024-02-04 16:07:04 +1000

    making the code look prettier (hopefully)

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-var counter = 0;
+let counter = 0;
 
-var audioFiles = ["audio/on.mp3", "audio/off.mp3"];
+let audioFiles = ["audio/on.mp3", "audio/off.mp3"];
 
-for (var i = 0; i < audioFiles.length; i++) {
-    var audio = new Audio(audioFiles[i]);
+for (let i = 0; i < audioFiles.length; i++) {
+    let audio = new Audio(audioFiles[i]);
     audio.load();
 }
 
-var img = new Image();
+let img = new Image();
 img.src = "images/on.svg";
 img.load();
 
 function pictureChange() {
 
 
-    var bulb = document.getElementById("lightbulb");
-    var text = document.getElementById("text");
-    var counterDisplay = document.getElementById("counter");
+    let bulb = document.getElementById("lightbulb");
+    let text = document.getElementById("text");
+    let counterDisplay = document.getElementById("counter");
 
     if (bulb.src.endsWith("images/on.svg")) {
         new Audio("audio/on.mp3").play();
