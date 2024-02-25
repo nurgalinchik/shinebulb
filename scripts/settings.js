325485ca0307ef74056e976fccf910c0e9e57dac
Andrew <aldortheold@gmail.com>
2024-02-25 10:37:48 +1000

    modified: scripts/settings.js & styles/style3.css

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,22 +1,21 @@
 const modeToggle = document.getElementById("modeToggle");
-const button = document.getElementById("modeToggle");
 let darkMode = localStorage.getItem("darkMode");
 
 const enableDarkMode = () => {
     document.body.classList.add("darkMode");
     localStorage.setItem("darkMode", "enabled");
-    button.innerHTML = "on";
+    modeToggle.innerHTML = "on";
 }
 
 const disableDarkMode = () => {
     document.body.classList.remove("darkMode");
     localStorage.setItem("darkMode", null);
-    button.innerHTML = "off";
+    modeToggle.innerHTML = "off";
 }
 
 if (darkMode === "enabled") {
     enableDarkMode();
-    button.innerHTML = "on";
+    modeToggle.innerHTML = "on";
 }
 
 modeToggle.addEventListener("click", () => {
